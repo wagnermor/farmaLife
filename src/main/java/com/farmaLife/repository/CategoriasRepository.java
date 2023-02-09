86658bcb743056c9616d74067efad7f091cb5f61
WagnerMor <wagnermor@outlook.com>
2023-02-09 13:34:46 -0300

    Fix: add @Repository in /repository/*

--- src/main/java/com/farmaLife/repository/CategoriasRepository.java
+++ src/main/java/com/farmaLife/repository/CategoriasRepository.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.farmaLife.model.Categorias;
-
+@Repository
 public interface CategoriasRepository extends JpaRepository<Categorias, Long>{
 	public List<Categorias> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
